f854b0f5bf73e8ec22154085a72b966de192ed9e
David Filipiak <df239@canterbury.ac.uk>
2020-10-16 11:22:39 +0100

    added components for entering username

--- SoftwareEngineeringApp/Form1.Designer.cs
+++ SoftwareEngineeringApp/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.usernameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(290, 224);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(139, 17);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Enter the Username:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(293, 256);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(136, 22);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // usernameButton
+            // 
+            this.usernameButton.Location = new System.Drawing.Point(293, 284);
+            this.usernameButton.Name = "usernameButton";
+            this.usernameButton.Size = new System.Drawing.Size(136, 34);
+            this.usernameButton.TabIndex = 3;
+            this.usernameButton.Text = "button1";
+            this.usernameButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.usernameButton);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Button usernameButton;
     }
 }
 
