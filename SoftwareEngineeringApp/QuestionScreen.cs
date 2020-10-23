dfbc52e618e9584eb8d976116d7325486784f872
David Filipiak <df239@canterbury.ac.uk>
2020-10-23 14:29:18 +0100

    added code for getting a random question

--- SoftwareEngineeringApp/QuestionScreen.cs
+++ SoftwareEngineeringApp/QuestionScreen.cs
@@ -12,9 +12,44 @@ namespace SoftwareEngineeringApp
 {
     public partial class QuestionScreen : Form
     {
+        private static int questionNumber = 1;
+        private static int difficulty; //data type can be changed
+        Question currentQuestion;
+     
+
         public QuestionScreen()
         {
             InitializeComponent();
+
+            DisplayQuestion(difficulty);
+        }
+
+        private void DisplayQuestion(int difficulty)
+        {
+            switch(difficulty)
+            {
+                case 1:
+                    currentQuestion = GetRandomQuestion(Quiz.questionsLvl1);
+                    break;
+                case 2:
+                    currentQuestion = GetRandomQuestion(Quiz.questionsLvl2);
+                    break;
+                case 3:
+                    currentQuestion = GetRandomQuestion(Quiz.questionsLvl3);
+                    break;
+                case 4:
+                    currentQuestion = GetRandomQuestion(Quiz.questionsLvl4);
+                    break;
+            }
+
+
+        }
+
+        private Question GetRandomQuestion(List<Question> questionList)
+        {
+            Random random = new Random();
+            int randomPos = random.Next(0, questionList.Count);
+            return questionList[randomPos];
         }
     }
 }
