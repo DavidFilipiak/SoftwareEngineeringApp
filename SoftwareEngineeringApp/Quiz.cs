c00a83e664df23d0d956b640f048ab4923aa1b16
David Filipiak <df239@canterbury.ac.uk>
2020-10-23 10:39:41 +0100

    added static class Quiz

--- SoftwareEngineeringApp/Quiz.cs
+++ SoftwareEngineeringApp/Quiz.cs
@@ -8,6 +8,24 @@ namespace SoftwareEngineeringApp
 {
     public static class Quiz
     {
+        //Lists of questions of all levels
+        public static List<Question> questionsLvl1 = new List<Question>();
+        public static List<Question> questionsLvl2 = new List<Question>();
+        public static List<Question> questionsLvl3 = new List<Question>();
+        public static List<Question> questionsLvl4 = new List<Question>();
+
+        public static Dictionary<string, int> highscores = new Dictionary<string, int>();
+        public static string currentUsername;
+
+        public static void LoadHighScoresFromFile()
+        {
+
+        }
+
+        public static void LoadQuestionsFromFile()
+        {
+
+        }
 
     }
 }
