d136ed3edc246dfb8f0de8e1b9f1168126633517
David Filipiak <df239@canterbury.ac.uk>
2020-11-17 18:44:18 +0000

    edited layout and controls of the question screen

--- SoftwareEngineeringApp/QuestionScreen.Designer.cs
+++ SoftwareEngineeringApp/QuestionScreen.Designer.cs
@@ -43,71 +43,99 @@
             this.helpNewQbutton = new System.Windows.Forms.Button();
             this.helpStopTimebutton = new System.Windows.Forms.Button();
             this.help5050button = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.helplinesGroupbox.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // optionA_button
             // 
-            this.optionA_button.Location = new System.Drawing.Point(12, 297);
+            this.optionA_button.AutoSize = true;
+            this.optionA_button.BackColor = System.Drawing.SystemColors.Info;
+            this.optionA_button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.optionA_button.Font = new System.Drawing.Font("Lucida Bright", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionA_button.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.optionA_button.Location = new System.Drawing.Point(3, 2);
             this.optionA_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.optionA_button.Name = "optionA_button";
-            this.optionA_button.Size = new System.Drawing.Size(600, 130);
+            this.optionA_button.Size = new System.Drawing.Size(626, 125);
             this.optionA_button.TabIndex = 0;
             this.optionA_button.Text = "button1";
-            this.optionA_button.UseVisualStyleBackColor = true;
+            this.optionA_button.UseVisualStyleBackColor = false;
             this.optionA_button.Click += new System.EventHandler(this.optionA_button_Click);
             // 
             // optionB_button
             // 
-            this.optionB_button.Location = new System.Drawing.Point(652, 297);
+            this.optionB_button.AutoSize = true;
+            this.optionB_button.BackColor = System.Drawing.SystemColors.Info;
+            this.optionB_button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.optionB_button.Font = new System.Drawing.Font("Lucida Bright", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionB_button.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.optionB_button.Location = new System.Drawing.Point(635, 2);
             this.optionB_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.optionB_button.Name = "optionB_button";
-            this.optionB_button.Size = new System.Drawing.Size(600, 130);
+            this.optionB_button.Size = new System.Drawing.Size(626, 125);
             this.optionB_button.TabIndex = 1;
             this.optionB_button.Text = "button2";
-            this.optionB_button.UseVisualStyleBackColor = true;
+            this.optionB_button.UseVisualStyleBackColor = false;
             this.optionB_button.Click += new System.EventHandler(this.optionB_button_Click);
             // 
             // optionC_button
             // 
-            this.optionC_button.Location = new System.Drawing.Point(12, 442);
+            this.optionC_button.AutoSize = true;
+            this.optionC_button.BackColor = System.Drawing.SystemColors.Info;
+            this.optionC_button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.optionC_button.Font = new System.Drawing.Font("Lucida Bright", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionC_button.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.optionC_button.Location = new System.Drawing.Point(3, 131);
             this.optionC_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.optionC_button.Name = "optionC_button";
-            this.optionC_button.Size = new System.Drawing.Size(600, 130);
+            this.optionC_button.Size = new System.Drawing.Size(626, 125);
             this.optionC_button.TabIndex = 2;
             this.optionC_button.Text = "button3";
-            this.optionC_button.UseVisualStyleBackColor = true;
+            this.optionC_button.UseVisualStyleBackColor = false;
             this.optionC_button.Click += new System.EventHandler(this.optionC_button_Click);
             // 
             // optionD_button
             // 
-            this.optionD_button.Location = new System.Drawing.Point(652, 442);
+            this.optionD_button.AutoSize = true;
+            this.optionD_button.BackColor = System.Drawing.SystemColors.Info;
+            this.optionD_button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.optionD_button.Font = new System.Drawing.Font("Lucida Bright", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionD_button.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.optionD_button.Location = new System.Drawing.Point(635, 131);
             this.optionD_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.optionD_button.Name = "optionD_button";
-            this.optionD_button.Size = new System.Drawing.Size(600, 130);
+            this.optionD_button.Size = new System.Drawing.Size(626, 125);
             this.optionD_button.TabIndex = 3;
             this.optionD_button.Text = "button4";
-            this.optionD_button.UseVisualStyleBackColor = true;
+            this.optionD_button.UseVisualStyleBackColor = false;
             this.optionD_button.Click += new System.EventHandler(this.optionD_button_Click);
             // 
             // question_label
             // 
-            this.question_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.question_label.AutoSize = true;
-            this.question_label.Location = new System.Drawing.Point(588, 223);
+            this.question_label.BackColor = System.Drawing.Color.Transparent;
+            this.question_label.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question_label.Location = new System.Drawing.Point(522, 223);
             this.question_label.Name = "question_label";
-            this.question_label.Size = new System.Drawing.Size(95, 17);
+            this.question_label.Size = new System.Drawing.Size(221, 34);
             this.question_label.TabIndex = 4;
             this.question_label.Text = "question here";
+            this.question_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // timer_label
             // 
+            this.timer_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.timer_label.AutoSize = true;
-            this.timer_label.Location = new System.Drawing.Point(1228, 9);
+            this.timer_label.BackColor = System.Drawing.Color.Transparent;
+            this.timer_label.Font = new System.Drawing.Font("Lucida Bright", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer_label.Location = new System.Drawing.Point(1185, 9);
             this.timer_label.Name = "timer_label";
-            this.timer_label.Size = new System.Drawing.Size(24, 17);
+            this.timer_label.Size = new System.Drawing.Size(67, 43);
             this.timer_label.TabIndex = 6;
             this.timer_label.Text = "20";
+            this.timer_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // timer
             // 
@@ -116,26 +144,32 @@
             // score_label
             // 
             this.score_label.AutoSize = true;
+            this.score_label.BackColor = System.Drawing.Color.Transparent;
+            this.score_label.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.score_label.Location = new System.Drawing.Point(16, 11);
             this.score_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.score_label.Name = "score_label";
-            this.score_label.Size = new System.Drawing.Size(49, 17);
+            this.score_label.Size = new System.Drawing.Size(104, 34);
             this.score_label.TabIndex = 7;
             this.score_label.Text = "Score:";
+            this.score_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // questionNumber_label
             // 
+            this.questionNumber_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.questionNumber_label.AutoSize = true;
-            this.questionNumber_label.Location = new System.Drawing.Point(387, 9);
+            this.questionNumber_label.BackColor = System.Drawing.Color.Transparent;
+            this.questionNumber_label.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionNumber_label.Location = new System.Drawing.Point(505, 9);
             this.questionNumber_label.Name = "questionNumber_label";
-            this.questionNumber_label.Size = new System.Drawing.Size(114, 17);
+            this.questionNumber_label.Size = new System.Drawing.Size(274, 34);
             this.questionNumber_label.TabIndex = 5;
             this.questionNumber_label.Text = "question number";
             // 
             // labelDiff
             // 
             this.labelDiff.AutoSize = true;
-            this.labelDiff.Location = new System.Drawing.Point(771, 9);
+            this.labelDiff.Location = new System.Drawing.Point(864, 11);
             this.labelDiff.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelDiff.Name = "labelDiff";
             this.labelDiff.Size = new System.Drawing.Size(126, 17);
@@ -144,9 +178,13 @@
             // 
             // helplinesGroupbox
             // 
+            this.helplinesGroupbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.helplinesGroupbox.BackColor = System.Drawing.Color.Transparent;
             this.helplinesGroupbox.Controls.Add(this.helpNewQbutton);
             this.helplinesGroupbox.Controls.Add(this.helpStopTimebutton);
             this.helplinesGroupbox.Controls.Add(this.help5050button);
+            this.helplinesGroupbox.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.helplinesGroupbox.Location = new System.Drawing.Point(12, 590);
             this.helplinesGroupbox.Name = "helplinesGroupbox";
             this.helplinesGroupbox.Size = new System.Drawing.Size(1240, 87);
@@ -156,54 +194,79 @@
             // 
             // helpNewQbutton
             // 
+            this.helpNewQbutton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.helpNewQbutton.Location = new System.Drawing.Point(969, 21);
             this.helpNewQbutton.Name = "helpNewQbutton";
             this.helpNewQbutton.Size = new System.Drawing.Size(206, 57);
             this.helpNewQbutton.TabIndex = 2;
             this.helpNewQbutton.Text = "New Question";
-            this.helpNewQbutton.UseVisualStyleBackColor = true;
+            this.helpNewQbutton.UseVisualStyleBackColor = false;
             this.helpNewQbutton.Click += new System.EventHandler(this.helpNewQbutton_Click);
             // 
             // helpStopTimebutton
             // 
+            this.helpStopTimebutton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.helpStopTimebutton.Location = new System.Drawing.Point(499, 21);
             this.helpStopTimebutton.Name = "helpStopTimebutton";
             this.helpStopTimebutton.Size = new System.Drawing.Size(220, 57);
             this.helpStopTimebutton.TabIndex = 1;
             this.helpStopTimebutton.Text = "Stop Timer";
-            this.helpStopTimebutton.UseVisualStyleBackColor = true;
+            this.helpStopTimebutton.UseVisualStyleBackColor = false;
             this.helpStopTimebutton.Click += new System.EventHandler(this.helpStopTimebutton_Click);
             // 
             // help5050button
             // 
+            this.help5050button.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.help5050button.Location = new System.Drawing.Point(77, 21);
             this.help5050button.Name = "help5050button";
             this.help5050button.Size = new System.Drawing.Size(200, 58);
             this.help5050button.TabIndex = 0;
             this.help5050button.Text = "50 / 50";
-            this.help5050button.UseVisualStyleBackColor = true;
+            this.help5050button.UseVisualStyleBackColor = false;
             this.help5050button.Click += new System.EventHandler(this.help5050button_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.optionA_button, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.optionC_button, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.optionB_button, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.optionD_button, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 292);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1264, 258);
+            this.tableLayoutPanel1.TabIndex = 9;
             // 
             // QuestionScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.helplinesGroupbox);
             this.Controls.Add(this.labelDiff);
             this.Controls.Add(this.score_label);
             this.Controls.Add(this.timer_label);
             this.Controls.Add(this.questionNumber_label);
             this.Controls.Add(this.question_label);
-            this.Controls.Add(this.optionD_button);
-            this.Controls.Add(this.optionC_button);
-            this.Controls.Add(this.optionB_button);
-            this.Controls.Add(this.optionA_button);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "QuestionScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "QuestionScreen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.QuestionScreen_FormClosing);
             this.helplinesGroupbox.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,5 +288,6 @@
         private System.Windows.Forms.Button helpNewQbutton;
         private System.Windows.Forms.Button helpStopTimebutton;
         private System.Windows.Forms.Button help5050button;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
