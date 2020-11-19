fa1359a009b1eb54702873f0ce60d3e88c8652b9
James Hopkins <jh1504@canterbury.ac.uk>
2020-11-19 18:50:58 +0000

    Edit vicscreen layout

--- SoftwareEngineeringApp/victoryScreen.Designer.cs
+++ SoftwareEngineeringApp/victoryScreen.Designer.cs
@@ -36,28 +36,31 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(570, 245);
+            this.button1.Location = new System.Drawing.Point(551, 283);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 35);
+            this.button1.Size = new System.Drawing.Size(159, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Home";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // listBox1
+            // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(932, 117);
-            this.listBox2.Name = "listBox1";
-            this.listBox2.Size = new System.Drawing.Size(135, 160);
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(911, 130);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(179, 196);
             this.listBox2.TabIndex = 1;
-           
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(98, 106);
+            this.button2.Location = new System.Drawing.Point(131, 130);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 32);
+            this.button2.Size = new System.Drawing.Size(153, 39);
             this.button2.TabIndex = 2;
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,13 +68,14 @@
             // 
             // victoryScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(1281, 681);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "victoryScreen";
             this.Text = "victoryScreen";
             this.ResumeLayout(false);
